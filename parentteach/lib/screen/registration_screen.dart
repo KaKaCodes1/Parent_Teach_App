2361399f6c4a3e7362494f08f3f6b26a8edab5ac
Ifeanyi222 <linfitechitsolcom@gmail.com>
2024-06-12 23:40:19 -0700

    setting up firebase for auth

--- parentteach/lib/screen/registration_screen.dart
+++ parentteach/lib/screen/registration_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:parentteach/service/firebase_auth_implementation/firebase_auth_services.dart';
+import 'solution_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -7,9 +10,20 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  String accountType = 'Parent'; // Default account type
+
+  final FirebaseAuthServices _auth = FirebaseAuthServices();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,32 +36,57 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         child: Column(
           children: [
             TextField(
+              controller: _emailController,
               keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
               decoration: InputDecoration(
                 hintText: 'Enter your email',
               ),
             ),
             TextField(
+              controller: _passwordController,
               obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
               decoration: InputDecoration(
                 hintText: 'Enter your password',
               ),
+            ),
+            DropdownButton<String>(
+              value: accountType,
+              items: <String>['Parent', 'Teacher'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  accountType = newValue!;
+                });
+              },
             ),
             ElevatedButton(
               onPressed: () async {
                 try {
                   final newUser = await _auth.createUserWithEmailAndPassword(
-                    email: email,
-                    password: password,
+                    email: _emailController.text,
+                    password: _passwordController.text,
                   );
                   if (newUser != null) {
+                    // Store account type information in Firestore
+                    await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(newUser.user!.uid)
+                        .set({
+                      'email': _emailController.text,
+                      'accountType': accountType,
+                    });
+
                     // Navigate to the next screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SolutionScreen(),
+                      ),
+                    );
                   }
                 } catch (e) {
                   print(e);
