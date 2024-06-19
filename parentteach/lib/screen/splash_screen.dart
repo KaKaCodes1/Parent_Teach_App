746c1098ed80e4b9156695b0c6a2a38d7870c0d4
Ifeanyi222 <linfitechitsolcom@gmail.com>
2024-06-19 22:55:24 +0100

    merge and updated

--- parentteach/lib/screen/splash_screen.dart
+++ parentteach/lib/screen/splash_screen.dart
@@ -10,31 +10,29 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
-    @override
-  void initState(){
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
     super.initState();
     //This removes the top and bottom phone icons like the back buttons and the status bar on top
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
     //how long the screen will be displayed before going to another page
-    Future.delayed(const Duration(seconds: 2), (){
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => const OnboardingScreen())
-      );
+          MaterialPageRoute(builder: (_) => const OnboardingScreen()));
     });
   }
 
-    @override
-  void dispose(){
+  @override
+  void dispose() {
     //This returns the icons
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-    overlays: SystemUiOverlay.values);
+        overlays: SystemUiOverlay.values);
 
     super.dispose();
-
   }
-  
 
   @override
   Widget build(BuildContext context) {
@@ -42,13 +40,13 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
-          gradient: RadialGradient(            center: FractionalOffset(0.5, 0.5),
-            focal: FractionalOffset(0.5, 0.5),
-            colors: [Color(0xffffe677), Color(0xff38b6ff)],
-            stops: [0.3, 1],
-            radius: 0.5,
-            )
-        ),
+            gradient: RadialGradient(
+          center: FractionalOffset(0.5, 0.5),
+          focal: FractionalOffset(0.5, 0.5),
+          colors: [Color(0xffffe677), Color(0xff38b6ff)],
+          stops: [0.3, 1],
+          radius: 0.5,
+        )),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
--- parentteach/lib/service/firebase_auth_implementation/firebase_auth_services.dart
+++ parentteach/lib/service/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,13 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthServices {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<User?> signUpWithEmailAndPassword(
-      String email, String password) async {
+    String email,
+    String password,
+    String fullName,
+    String username,
+    String location,
+    String dateOfBirth,
+  ) async {
     try {
       UserCredential credential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
+      User? user = credential.user;
+      if (User != null) {
+        // Store additional user information in Firestore
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user?.uid)
+            .set({
+          'fullName': fullName,
+          'username': username,
+          'location': location,
+          'dateOfBirth': dateOfBirth,
+          'email': email,
+        });
+      }
       return credential.user;
     } catch (e) {
       print("Error Occured");
